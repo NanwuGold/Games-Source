cab6e1bd5e882668f733ef9f0341a6033b69a48b
南无仙 <granbao@gmail.com>
2025-03-22 22:39:51 +0800

    add Games 101 HW7

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@
 @REM cmake .\Games101\HW4\src\ -B ./build/Games101/HW4 -GNinja -DCMAKE_BUILD_TYPE="Debug"
 @REM cmake .\Games101\HW5\src\ -B ./build/Games101/HW5 -GNinja -DCMAKE_BUILD_TYPE="Debug"
 
-cmake .\Games101\HW6\src\ -B ./build/Games101/HW6 -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=clang
+@rem cmake .\Games101\HW6\src\ -B ./build/Games101/HW6 -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=clang
+cmake .\Games101\HW7\src\ -B ./build/Games101/HW7 -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=clang
 
 @REM echo off
 @REM rm ./compile_commands.json -r
